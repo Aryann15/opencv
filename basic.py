d3d6e97b5e597d9d803415456de5c31832cd5d24
Aryann15 <rajlysm15@gmail.com>
2023-07-05 01:32:22 +0530

    edge cascade, dilating, erode, resize,  cropping

--- basic.py
+++ basic.py
@@ -10,5 +10,26 @@ cv.imshow('grayscale',gray)
 # Blur
 blur = cv.GaussianBlur(img,(3,3), cv.BORDER_DEFAULT)
 cv.imshow('Blur', blur)
+
+#Edge cascade
+canny = cv.Canny(blur  ,125,175)
+cv.imshow('Canny edges', canny)
+
+#dilating the images
+dilated= cv.dilate(canny, (3,3),iterations=1)
+cv.imshow('dilated', dilated)
+
+#eroded
+eroded = cv.erode(dilated,(3,3),iterations= 1)
+cv.imshow('eroded',eroded)
+
+#Resize
+
+resize = cv.resize(img,(500,500))
+cv.imshow('resized',resize)
+
+#cropping
+cropped = img[50:200 , 200:400]
+cv.imshow('cropped',cropped)
 cv.waitKey(0)
 
