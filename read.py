f23750a8086e7d0f27b009be7759d9f949fd8c48
Aryann15 <rajlysm15@gmail.com>
2023-07-05 00:06:49 +0530

    resizing and rescaling frames

--- read.py
+++ read.py
@@ -5,15 +5,39 @@ import cv2 as cv
 # cv.imshow('1st Cat', img)
 # cv.waitKey(0)
 
-#READING VIDEOS
+# READING VIDEOS
+#
+# capture = cv.VideoCapture('Videos/naruto1.gif')
+#
+# while True:
+#     isTrue,frame = capture.read()
+#     cv.imshow('Video',frame)
+#     if cv.waitKey(20) &0xFF==ord('d'):
+#         break
+#
+# capture.release()
+# cv.destroyAllWindows()
 
-capture = cv.VideoCapture('Videos/naruto1.gif')
+# resizing and rescaling frames
 
-while True:
-    isTrue,frame = capture.read()
-    cv.imshow('Video',frame)
-    if cv.waitKey(20) &0xFF==ord('d'):
-        break
+import cv2 as cv
 
-capture.release()
-cv.destroyAllWindows()
+img = cv.imread('Photos/cat1.jpeg')
+cv.imshow('1st Cat', img)
+
+def rescaleFrame (frame, scale=0.75):
+    # photo, video , live video
+    width = int(frame.shape[1]* scale)
+    height= int(frame.shape[0]* scale)
+
+    dimensions = (width,height)
+
+    return cv.resize(frame,dimensions, interpolation = cv.INTER_AREA )
+def changeRes (width,height):
+    #live videos
+    capture.set(3,width)
+    capture.set(4,height)
+
+frame_resized =rescaleFrame(img)
+cv.imshow('frame resized', frame_resized )
+cv.waitKey(0)
