e064a430ae901c8ba516b002cf5f1650b850b2f2
Aryann15 <rajlysm15@gmail.com>
2023-07-04 23:34:57 +0530

    read videos

--- read.py
+++ read.py
@@ -1,7 +1,19 @@
 import cv2 as cv
 
-img = cv.imread('Photos/cat1.jpeg')
+# img = cv.imread('Photos/cat1.jpeg')
+#
+# cv.imshow('1st Cat', img)
+# cv.waitKey(0)
 
-cv.imshow('1st Cat', img)
+#READING VIDEOS
 
-cv.waitKey(0)
+capture = cv.VideoCapture('Videos/naruto1.gif')
+
+while True:
+    isTrue,frame = capture.read()
+    cv.imshow('Video',frame)
+    if cv.waitKey(20) &0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
